33a8baeb81e23058ec29fa83eac7989d99e908a0
vivekdhir77 <vivekdhir77@gmail.com>
2021-05-16 20:13:37 +0530

    changed

---
+++ A. Potion making.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+typedef long int li;
+typedef long long int ll;
+#define max(a, b) (a > b ? a : b)
+#define min(a, b) (a > b ? b : a)
+#define FA(i, start, end) for (int i = start; i < end; i++)
+#define FD(i, start, end) for (int i = start; i >= end; i--)
+#define VIVEK_DHIR ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define in(t) ll t;cin>>t;
+#define tc(t) while(t--)
+
+void solve()
+{
+    in(k);
+    float e = 
+    float a = 100.0/k;
+    int b = 100/k;
+    if ((a-b)==0)
+    cout<<b<<endl;
+    else
+    cout<<"100"<<endl;
+}
+
+int main()
+{
+    VIVEK_DHIR
+// #ifndef ONLINE_JUDGE
+//     freopen("in.txt", "r", stdin);
+//     freopen("out.txt", "w", stdout);
+//     freopen("error.txt", "w", stderr);
+// #endif
+    in(t);
+    tc(t)
+    solve();
+}
